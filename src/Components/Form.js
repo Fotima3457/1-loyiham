7fa2a0e57e9f66cad29740388fd5b319508896fb
Fotima <asliddinabduqahhorov5@gmail.com>
2023-01-10 14:02:46 +0500

    added style

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -9,11 +9,11 @@ const Salom = () => {
     <div>
     <Form className='orm'>
     <Form.Group id='nam' className="mb-3" controlId="formBasicEmail">
-      <Form.Label >Name:</Form.Label>
-      <Form.Control type="text" placeholder="Name..." />
+      <Form.Label style={{color:"black"}} >Name:</Form.Label>
+      <Form.Control style={{color:"black"}} type="text" placeholder="Name..." />
     </Form.Group>
     <Form.Group id='phone' className="mb-3" controlId="formBasicPassword">
-      <Form.Label >Phone number:</Form.Label>
+      <Form.Label style={{color:"black"}} >Phone number:</Form.Label>
       <Form.Control type="phone" placeholder="Phone number..." />
     </Form.Group>
    <Link  to="/home">  <Button id='btn'  variant="primary" type="submit">  Next </Button></Link>
